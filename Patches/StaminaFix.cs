62e68e5131a6af0dc96d2cb09749f7d86f1d62bb
SunsetOS <weihou.huang@gmail.com>
2024-11-01 18:28:29 +0800

    Upload

---
+++ Patches/StaminaFix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using GameNetcodeStuff;
+using UnityEngine;
+
+namespace LCStaminaFix.Patches
+{
+    [HarmonyPatch(typeof(GameNetcodeStuff.PlayerControllerB))]
+
+    internal class StaminaFix
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void Postfix(ref float ___sprintMeter, ref bool ___isSprinting, ref bool ___isWalking, ref float ___sprintTime)
+        {
+            if (!___isSprinting && !___isWalking)
+            {
+                ___sprintMeter = Mathf.Clamp(___sprintMeter + 0.08f * Time.deltaTime - Time.deltaTime / (___sprintTime + 4f), 0f, 1f);
+            }else if (!___isSprinting && ___isWalking)
+            {
+                ___sprintMeter = Mathf.Clamp(___sprintMeter + 0.07f * Time.deltaTime - Time.deltaTime / (___sprintTime + 9f), 0f, 1f);
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using LCStaminaFix.Patches;
+
+namespace LCStaminaFix
+{
+
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class LCStaminaFix : BaseUnityPlugin
+    {
+
+        public const string PLUGIN_GUID = "sunsetos.lcstaminafix";
+        public const string PLUGIN_NAME = "lcstaminafix";
+        public const string PLUGIN_VERSION = "1.0.1";
+
+
+        // private static ConfigFile configFile;
+        private readonly Harmony harmony = new Harmony(PLUGIN_GUID);
+
+        private static LCStaminaFix Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(PLUGIN_GUID);
+            mls.LogInfo(PLUGIN_GUID + " is here");
+
+            harmony.PatchAll(typeof(StaminaFix));
+            harmony.PatchAll(typeof(LCStaminaFix));
+        }
+
+        
+    }
+    
+
+    
+}
